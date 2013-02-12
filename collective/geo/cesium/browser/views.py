4324613863e49a1c6b3958bc68b3027e71ddb9a6
Christian Ledermann <christian.ledermann@gmail.com>
2013-02-12 18:45:15 +0300

    add a layerswitcher for bing and osm

--- collective/geo/cesium/browser/views.py
+++ collective/geo/cesium/browser/views.py
@@ -1,14 +1,29 @@
 from zope.interface import implements, Interface
+from zope.component import getUtility
 
 from Products.Five import BrowserView
+from plone.registry.interfaces import IRegistry
+from collective.geo.settings.interfaces import IGeoSettings
 
 class ICesiumTestView(Interface):
     """
-    File Kml view interface
+    cesium test view interface
     """
 
 class CesiumTestView(BrowserView):
     """
-    browser view to display a map for a kml file
+    browser view to display a cesium globe map
     """
     implements(ICesiumTestView)
+
+    def get_baselayers(self):
+        default_layers = self.geo_settings.default_layers
+        #import ipdb; ipdb.set_trace()
+        return ''
+
+
+    @property
+    def geo_settings(self):
+        return getUtility(IRegistry).forInterface(IGeoSettings)
+
+
